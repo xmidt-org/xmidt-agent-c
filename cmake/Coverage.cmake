68bf0f11f3d801bd80b32c0fbe5d76d5fe3aaf05
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2021-06-17 02:50:00 -0700

    Update the coverage check to look for the lcov program.  Also update curl verion needed.

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -1,6 +1,17 @@
 # SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+find_program(LCOV_BIN lcov)
+find_program(GENINFO_BIN geninfo)
+find_program(GENHTML_BIN genhtml)
+
+if (NOT LCOV_BIN)
+    message(STATUS "lcov not found, coverage report not supported.")
+    return()
+endif (NOT LCOV_BIN)
+
+message(STATUS "Found lcov")
+
 add_custom_target(coverage
                   COMMAND lcov -q --capture
                           --directory ${CMAKE_CURRENT_BINARY_DIR}/

dd2a7d018ed025fbf11cb09d0605c2410b13f3f7
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2021-06-21 22:27:49 -0700

    Don't try to checkout curl.

--- cmake/FindCurl.cmake
+++ cmake/FindCurl.cmake
@@ -23,14 +23,8 @@ function(find_curl)
 
     if (NOT DEFINED CURL_VERSION)
         set(CURL_ENFORCE_VERSION "libcurl")
-        if (NOT DEFINED CURL_GIT_TAG)
-            set(CURL_GIT_TAG "")
-        endif()
     else ()
         set(CURL_ENFORCE_VERSION "libcurl>=${CURL_VERSION}")
-        if (NOT DEFINED CURL_GIT_TAG)
-            set(CURL_GIT_TAG "v${CURL_VERSION}")
-        endif()
     endif()
 
     find_path(CURL_INCLUDE_DIR
@@ -57,19 +51,7 @@ function(find_curl)
             message(STATUS "Using system provided libcurl (found version \"${CURL_VERSION}\")")
             include_directories(SYSTEM ${CURL_INCLUDE_DIRS})
         else()
-            include(ExternalProject)
-
-            message(STATUS "Fetching upstream curl (tag \"${CURL_GIT_TAG}\")")
-            ExternalProject_Add(curl
-                PREFIX ${LOCAL_PREFIX_DIR}/curl
-                GIT_REPOSITORY https://github.com/xmidt-org/curl.git
-                GIT_TAG ${CURL_GIT_TAG}
-                CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LOCAL_INSTALL_DIR} -DBUILD_TESTING=OFF)
-            add_library(libcurl STATIC IMPORTED)
-            add_dependencies(libcurl curl)
-            add_dependencies(${CMAKE_PROJECT_NAME} curl)
-            include_directories(SYSTEM ${LOCAL_INCLUDE_DIR})
-            set(CURL_LIBRARIES "${LOCAL_LIBRARY_DIR}/libcurl.so" PARENT_SCOPE)
+            message(FATAL_ERROR "Unable to find libcurl.")
         endif()
     endif()
 endfunction()

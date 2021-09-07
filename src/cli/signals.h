8f36de5f9474d2b7badfc6ccb77676dc127c5ea9
Weston Schmidt <weston_schmidt@alumni.purdue.edu>
2021-09-06 22:36:25 -0700

    Add some basic signal handling. (#17)
    * Add some basic signal handling.
    
    * Remove a code smell and fix a minor formatting issue.

---
+++ src/cli/signals.h
@@ -0,0 +1,20 @@
+/* SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __SIGNALS_H__
+#define __SIGNALS_H__
+
+
+enum signals_command {
+    SIG_EVENT__STOP,
+    SIG_EVENT__TERMINATE,
+    SIG_EVENT__SYSTEM_IS_RESTARTING
+};
+
+/**
+ *  config_signals() configures the signals to call the provided handler with
+ *  the information about what the system is telling us to do.
+ */
+void signals_config(void (*command)(enum signals_command));
+
+#endif
